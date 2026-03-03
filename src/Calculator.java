dee9eae4552d6d7062ed8da88eb725362f7dd999
hanyejin <biziyejin8844@naver.com>
2026-03-03 18:01:58 +0900

    계산기

---
+++ src/Calculator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int first=0,second=0;
+
+        System.out.println("=== Java 계산기 ===");
+
+        System.out.println("첫 번째 숫자: ");
+        /*int first = new Scanner.nextInt();
+        System.out.println("연산자를 입력하세요(+, -, *, /): ");
+        String Symbol=new Scanner.nextLine();
+        System.out.println("두 번째 숫자: ");
+        int second = new Scanner.nextInt();
+        if(first)*/
+
+        System.out.println("계산기를 종료합니다.");
+        scanner.close();
+    }
+}
