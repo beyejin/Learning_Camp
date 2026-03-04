3ed378cfcd11f843461ba9e246b78c8a5a688fd3
hanyejin <biziyejin8844@naver.com>
2026-03-04 16:32:00 +0900

    계산기

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,21 +2,23 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         Calculator calculator = new Calculator();
+        double result = 0;
 
         System.out.println("=== Java 계산기 ===");
         String ans="y";
 
         while(ans.equals("y")){
             System.out.print("첫 번째 숫자: ");
-            int first = scanner.nextInt();
+            double first = scanner.nextInt();
 
             System.out.print("연산자를 입력하세요(+, -, *, /): ");
             String symbol= scanner.next();
 
             System.out.print("두 번째 숫자: ");
-            int second = scanner.nextInt();
+            double second = scanner.nextInt();
 
 
             if(symbol.equals("+")){
@@ -29,8 +31,7 @@ public class Calculator {
                 calculator.multiply(first, second);
             }
             else if(symbol.equals("/")){
-                int result=calculator.divide(first, second);
-                System.out.println("결과: "+first+"/"+second+"="+result);
+                calculator.divide(first, second);
             }
             else{
                 System.out.println("잘못된 연산자입니다.");
@@ -39,30 +40,31 @@ public class Calculator {
             ans= scanner.next();
 
 
-        };
+        }
         System.out.println("계산기를 종료합니다.");
         scanner.close();
 
     }
-    void sum(int value1, int value2) {
-        int result = value1 + value2;
+    void sum(double value1, double value2) {
+        double result = value1 + value2;
         System.out.println("결과: "+value1+"+"+value2+"="+result);
     }
-    void subtract(int value1, int value2) {
-        int result = value1 - value2;
+    void subtract(double value1, double value2) {
+        double result = value1 - value2;
         System.out.println("결과: "+value1+"-"+value2+"="+result);
     }
-    void multiply(int value1, int value2){
-        int result= value1 * value2;
+    void multiply(double value1, double value2){
+        double result= value1 * value2;
         System.out.println("결과: "+value1+"*"+value2+"="+result);
     }
 
-    int divide(int value1, int value2){
+    void divide(double value1, double value2){
         if(value2==0){
             System.out.println("0으로 나눌 수 없습니다.");
-            return 0;
+            return;
         }
-        return value1 /value2;
 
+        double result = value1 / value2;
+        System.out.println("결과: "+value1+"/"+value2+"="+result);
     }
 }
